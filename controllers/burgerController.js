0396640eec1ecd4a26ec44c78c11560ad9cfffa6
Carlito Algarin <carlito1113@yahoo.com>
2020-10-18 22:02:59 -0400

    added in an onclick event function into my index.handlebars

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -17,7 +17,7 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-  cat.create([
+  burger.create([
     "name", "sleepy"
   ], [
     req.body.name, req.body.sleepy
